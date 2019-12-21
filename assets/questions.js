7a942229704281c3b8def0b2c9b35955c5ea31c5
Rachel Iaconianni <iaconiannir@gmail.com>
2019-12-21 14:38:25 -0500

    added back all questions

--- assets/questions.js
+++ assets/questions.js
@@ -3,60 +3,60 @@ var questions = [
     questions: "Who created JavaScript?",
     choices: ["Bill Gates", "Brendan Eich", "Steve Jobs", "Bjarne Stroustrup"],
     answer: "Brendan Eich"
+  },
+  {
+    questions: "Which is NOT a JavaScript data type?",
+    choices: ["Boolean", "String", "Object", "API"],
+    answer: "API"
+  },
+  {
+    questions: "What does NaN stand for?",
+    choices: ["Not a noun", "Not a number", "Not a null", "Not a note"],
+    answer: "Not a number"
+  },
+  {
+    questions: "What does Boolean represent?",
+    choices: ["True", "False", "Neither", "Both"],
+    answer: "Both"
+  },
+  {
+    questions: "What is an array?",
+    choices: ["A variable that can hold more than one value at a time", "A conditional statement", "A get method", "A variable that can only hold one value at a time"],
+    answer: "A variable that can hold more than one value at a time"
+  },
+  {
+    questions: "What DOM event is most commonly used with a button?",
+    choices: ["Mouseover", "Keyup", "Select", "Click"],
+    answer: "Click"
+  },
+  {
+    questions: "What HTML tag do you use for Javascript?",
+    choices: ["<src>", "<script>", "<img>", "<link>"],
+    answer: "<script>"
+  },
+  {
+    questions: "When saving a JavaScript file, what does it need to have?",
+    choices: [".js", ".css", ".html", ".json"],
+    answer: ".js"
+  },
+  {
+    questions: "True or False: == is the same as ===",
+    choices: ["True", "False"],
+    answer: "False"
+  },
+  {
+    questions: "What are the types of popup boxes that you can use in JavaScript?",
+    choices: ["Prompt", "Alert", "Confirm", "All the above"],
+    answer: "All the above"
+  },
+  {
+    questions: "What JavaScript method removes the last element of an array and returns it?",
+    choices: [".push", ".pop", ".parseInt", ".indexOf"],
+    answer: ".pop"
+  },
+  {
+    questions: "Why do programmers use console.log?",
+    choices: ["It makes their code look cool", "Code won't run without it", "It helps coders determine what the code is doing", "It determines if a value is true or false"],
+    answer: "It helps coders determine what the code is doing"
   }
-  // {
-  //   questions: "Which is NOT a JavaScript data type?",
-  //   choices: ["Boolean", "String", "Object", "API"],
-  //   answer: "API"
-  // },
-  // {
-  //   questions: "What does NaN stand for?",
-  //   choices: ["Not a noun", "Not a number", "Not a null", "Not a note"],
-  //   answer: "Not a number"
-  // },
-  // {
-  //   questions: "What does Boolean represent?",
-  //   choices: ["True", "False", "Neither", "Both"],
-  //   answer: "Both"
-  // },
-  // {
-  //   questions: "What is an array?",
-  //   choices: ["A variable that can hold more than one value at a time", "A conditional statement", "A get method", "A variable that can only hold one value at a time"],
-  //   answer: "A variable that can hold more than one value at a time"
-  // },
-  // {
-  //   questions: "What DOM event is most commonly used with a button?",
-  //   choices: ["Mouseover", "Keyup", "Select", "Click"],
-  //   answer: "Click"
-  // },
-  // {
-  //   questions: "What HTML tag do you use for Javascript?",
-  //   choices: ["<src>", "<script>", "<img>", "<link>"],
-  //   answer: "<script>"
-  // },
-  // {
-  //   questions: "When saving a JavaScript file, what does it need to have?",
-  //   choices: [".js", ".css", ".html", ".json"],
-  //   answer: ".js"
-  // },
-  // {
-  //   questions: "True or False: == is the same as ===",
-  //   choices: ["True", "False"],
-  //   answer: "False"
-  // },
-  // {
-  //   questions: "What are the types of popup boxes that you can use in JavaScript?",
-  //   choices: ["Prompt", "Alert", "Confirm", "All the above"],
-  //   answer: "All the above"
-  // },
-  // {
-  //   questions: "What JavaScript method removes the last element of an array and returns it?",
-  //   choices: [".push", ".pop", ".parseInt", ".indexOf"],
-  //   answer: ".pop"
-  // },
-  // {
-  //   questions: "Why do programmers use console.log?",
-  //   choices: ["It makes their code look cool", "Code won't run without it", "It helps coders determine what the code is doing", "It determines if a value is true or false"],
-  //   answer: "It helps coders determine what the code is doing"
-  // }
   ];
